9e00db52315027a26ccbe256238a87afa61b0855
Riswick <riswickelvariste@gmail.com>
2025-01-12 11:23:58 -0400

    add folder

---
+++ j.s/function.js
@@ -0,0 +1,12 @@
+function moyenne(jessica, keicha, guerda) {
+  let calcule = jessica + keicha + guerda;
+  //console.log(calcule);
+}
+
+moyenne(12, 23, 54);
+
+function rectagleRectangleArea(longeur, larger) {
+  let note = longeur * larger;
+  console.log(note);
+}
+rectagleRectangleArea(23, 43);
